276169904cb6064a3bc7db5a71033f532a6fd1ce
LevkoBe <lbeniakh@kse.org.ua>
2024-04-10 18:56:38 +0300

    paralellization

--- GameTraining.py
+++ GameTraining.py
@@ -1,5 +1,6 @@
 
 import copy
+import multiprocessing
 from GameFlow import GameFlow
 from Gameset import Gameset
 
@@ -24,7 +25,7 @@ from Gameset import Gameset
 
 
 class GeneticAlgorithm:
-    def __init__(self, initial_gameset: Gameset, num_games_in_generation: int = 10, num_best_children: int = 5, mutation_rate: float = 0.5):
+    def __init__(self, initial_gameset: Gameset, num_games_in_generation: int = 10, num_best_children: int = 5, mutation_rate: float = 0.2):
         self.initial_gameset = initial_gameset
         self.num_games_in_generation = num_games_in_generation
         self.num_best_children = num_best_children
@@ -45,12 +46,17 @@ class GeneticAlgorithm:
 
     def play_a_game(self, num_moves, current_generation: list[GameFlow]):
         while True:
-            # Play moves for each gameset in the current generation
-            games_to_remove = []
+            # Define a multiprocessing pool
+            pool = multiprocessing.Pool()
 
-            for game in current_generation:
-                game.play_game(num_moves)
-                winner = game.game_finished()
+            # Play moves for each game flow in the current generation in parallel
+            results = pool.map(self.play_moves, [(game, num_moves) for game in current_generation])
+            pool.close()
+            pool.join()
+
+            # Process the results
+            games_to_remove = []
+            for game, winner in results:
                 if winner == 2:  # black won
                     self.update_coefficients(game.game.coefficients)
                     return
@@ -61,7 +67,7 @@ class GeneticAlgorithm:
 
             for game in games_to_remove:
                 current_generation.remove(game)
-        
+            
             # if we lose in every game flow, start again
             if not current_generation:
                 return
@@ -69,6 +75,8 @@ class GeneticAlgorithm:
             # Select the best gamesets to retain, based on evaluation with original coefficients, and update the original coefficients
             sorted_generation = sorted(current_generation, key=lambda x: x.game.board.evaluate_position(x.white_pieces, x.black_pieces, x.game.piece_mapping, self.initial_gameset.coefficients))
             best_gamesets = sorted_generation[:self.num_best_children]
+            print(f"Best results: {[game.game.board.evaluate_position(game.white_pieces, game.black_pieces, game.game.piece_mapping, self.initial_gameset.coefficients) for game in best_gamesets]}")
+            print(f"Best coefficients: {[game.game.coefficients for game in best_gamesets]}")
             self.update_coefficients(best_gamesets[0].game.coefficients)
             
             children = []
@@ -81,3 +89,10 @@ class GeneticAlgorithm:
         
             # Update current generation with the best gamesets and children
             current_generation = best_gamesets + children
+    
+    def play_moves(self, args):
+        game, num_moves = args
+        game.play_game(num_moves)
+        winner = game.game_finished()
+        return game, winner
+    
